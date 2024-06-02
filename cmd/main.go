7a2aa684e947d5e8aabc7cf8e651793f91091fff
Mikhail Stasyuk <mihail.stasyuk2014@yandex.ru>
2024-06-02 12:04:58 +0500

    Project init

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", nil)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
